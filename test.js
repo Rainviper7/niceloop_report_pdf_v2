9d4d35859713b7cfb2ee7ebfaf5f4f6276be3fe6
Pongsakorn  Moonwang <pongsakorn.mon@gmail.com>
2017-09-25 12:11:47 +0700

    copy patthen

--- test.js
+++ test.js
@@ -8,34 +8,77 @@ var now = new Date(),
     datetime = moment(now).format("DDMMYY_HHmmSSS")
     ;
 
-//--------product--------
-var mockdata = require('./libs/product_report/mock_data').data,
-    productPdf = require('./libs/product_report/index')
-    ;
-//--------product--------
+//----main---
+voidreport();
 
-var params = {},
-    prefix_name = "product",
-    ShopName = mockdata.ShopName||"Niceloop Test Lab",
-    params_default = {
-        filePath: '/tmp/' + filename, //cloud
-        data: {},
-        from: '2017-03-01T15:00:00',
-        to: '2017-03-31T15:00:00',
-        shopname: "" || ShopName,
-        customerId: '4073'
-    };
-    var filename = './output/' + prefix_name + '_' + ShopName + '_' + datetime + '.pdf'
-    ;
-
-//-----------in dev mode
-params_default.filePath = './output/' + prefix_name + '_' + ShopName + '_' + datetime + '.pdf'
-params_default.data = mockdata;
-params = _.assign({}, params_default)
 
 //-------------------
-new productPdf.Report(params, function (filePath) {
-    console.log("Gen file pdf complete : " + filePath)
-});
+function dailyemail() {
+
+    //--------dailyEmail--------
+    var mockdata = require('./libs/daily_email_report/mock_data').data,
+        dailyEmailPdf = require('./libs/daily_email_report/index')
+        ;
+    //--------dailyEmail--------
+
+    var params = {},
+        prefix_name = "daily_email",
+        ShopName = mockdata.ShopName || "Niceloop Test Lab",
+        filename = './output/' + prefix_name[1] + '_' + ShopName + '_' + datetime + '.pdf',
+        params_default = {
+            filePath: '/tmp/' + filename, //cloud
+            data: {},
+            from: '2017-03-01T15:00:00',
+            to: '2017-03-31T15:00:00',
+            shopname: "" || ShopName,
+            customerId: '4073'
+        };
+    //-----------in dev mode
+    if (process.env.DEV_MODE == 'true') {
+        params_default.shopname = "Niceloop Test Lab"
+        params_default.filePath = filename
+        params_default.data = mockdata;
+        params = _.assign({}, params_default)
+
+    }
+
+    new dailyEmailPdf.Report(params, function (filePath) {
+        console.log("Gen file pdf complete : " + filePath)
+    });
+}
 
 
+
+function voidreport() {
+
+    //--------void--------
+    var voidReport = require('./libs/void_report/index')
+        ;
+    //--------void--------
+
+    var params = {},
+        prefix_name = "void",
+        ShopName = mockdata.ShopName || "Niceloop Test Lab",
+        filename = './output/' + prefix_name + '_' + ShopName + '_' + datetime + '.pdf',
+        params_default = {
+            filePath: '/tmp/' + filename, //cloud
+            data: {},
+            from: '2017-03-01T15:00:00',
+            to: '2017-03-31T15:00:00',
+            shopname: "" || ShopName,
+            customerId: '4073'
+        };
+
+    //-----------in dev mode
+    if (process.env.DEV_MODE == 'true') {
+        params_default.shopname = "Niceloop Test Lab"
+        params_default.filePath = filename
+        params_default.data = mockdata;
+        params = _.assign({}, params_default)
+
+    }
+
+    new voidReport.Report(params, function (filePath) {
+        console.log("Gen file pdf complete : " + filePath)
+    });
+}
